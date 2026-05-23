493c7ba87d25649aec814c159bb7b970fcfd91af
onistudz <adrien.cerrone@isen.yncrea.fr>
2026-05-23 04:23:27 +0200

    Add favicon and update manifest for improved PWA support

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'our-story-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
